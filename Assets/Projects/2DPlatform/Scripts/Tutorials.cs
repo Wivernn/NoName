3051908e0f26c55978a5f87aad24f9b41b54f4c9
ERU1220 SC <ERU1220SC@yoobeestudent.ac.nz>
2024-09-10 14:36:17 +1200

    tut. remake // finished w. tileset on scene 2

--- Assets/Projects/2DPlatform/Scripts/Tutorials.cs
+++ Assets/Projects/2DPlatform/Scripts/Tutorials.cs
@@ -6,12 +6,12 @@ using UnityEngine;
 public class Tutorials : MonoBehaviour
 {
     //idea: have transform for each tutorial block, and set so that when player enters a specific block, the tip pops up in the corner
-    public GameObject leftRight;
-    public P2DPlayerMoveController moveController;
+    //public GameObject leftRight;
+    //public P2DPlayerMoveController moveController;
 
     private void Update()
     {
-        if (moveController.walked == true)
+        /*if (moveController.walked == true)
         {
             StartCoroutine(start(3));
         }
@@ -19,13 +19,22 @@ public class Tutorials : MonoBehaviour
         if (moveController.jumped == true)
         {
             StartCoroutine(start(3));
+        }*/
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "player") 
+        {
+            Debug.Log("asjk");
+        //GameObject.Destroy(leftRight);
         }
     }
-   
-    IEnumerator start(float Cooldown)
-    {
-        yield return new WaitForSeconds(3);
-        GameObject.Destroy(leftRight);
-    }
+
+    /* IEnumerator start(float Cooldown)
+     {
+         yield return new WaitForSeconds(3);
+         GameObject.Destroy(leftRight);
+     }*/
 
 }
