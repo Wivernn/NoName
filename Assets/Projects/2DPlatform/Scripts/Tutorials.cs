e7a9a9330acfa023539e4b31cc29ccb348d34af1
ERU1220 SC <ERU1220SC@yoobeestudent.ac.nz>
2024-09-24 14:20:24 +1200

    tutorial fin.

--- Assets/Projects/2DPlatform/Scripts/Tutorials.cs
+++ Assets/Projects/2DPlatform/Scripts/Tutorials.cs
@@ -1,40 +1,129 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Tutorials : MonoBehaviour
 {
-    //idea: have transform for each tutorial block, and set so that when player enters a specific block, the tip pops up in the corner
-    //public GameObject leftRight;
-    //public P2DPlayerMoveController moveController;
+    public AudioSource Victory;
+    public GameObject enemy1;
+    public GameObject enemy2;
+
+    int runOnce1 = 0;
+    int runOnce2 = 0;
+
+    public GameObject Point1, Point2, Point3, Point4;   
+    public GameObject one, two, three, four, five;
+    public bool One, Two, Three, Four, Five;
+
+    Coroutine coroutine;
+
+
+
+    private void Start()
+    {
+        enemy1 = GameObject.Find("Enemy (1)");
+        enemy2 = GameObject.Find("Enemy (2)");
+
+
+        Point1 = GameObject.Find("Point1");
+        Point2 = GameObject.Find("Point2");
+        Point3 = GameObject.Find("Point3");
+        Point4 = GameObject.Find("Point4");
+
+        
+
+    }
 
     private void Update()
     {
-        /*if (moveController.walked == true)
-        {
-            StartCoroutine(start(3));
-        }
+        if (runOnce1 == 0) {
+            if (enemy1 == null)
+            {
+                runOnce1++;
+                Victory.Play();
 
-        if (moveController.jumped == true)
+                if (Five == false)
+                {
+                    Five = true;
+                    coroutine = StartCoroutine(Destroy(1));
+                }
+            }
+        }
+        if (runOnce2 == 0)
         {
-            StartCoroutine(start(3));
-        }*/
+            if (enemy2 == null)
+            {
+                runOnce2++;
+                Victory.Play();
+
+                if (Five == false)
+                {
+                    Five = true;
+                    coroutine = StartCoroutine(Destroy(1));
+                }
+            }
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "player") 
+        if (collision.tag == "Success")
         {
-            Debug.Log("asjk");
-        //GameObject.Destroy(leftRight);
+            Victory.Play();
+            coroutine = StartCoroutine(Destroy(1));
+        }
+        if (collision.gameObject == Point1)
+        {
+            One = true;
+        }
+        if (collision.gameObject == Point2)
+        {
+            Two = true;
+        }
+        if (collision.gameObject == Point3)
+        {
+            Three = true;
+        }
+        if (collision.gameObject == Point4)
+        {
+            Four = true;
+        }
+
+    }
+
+    IEnumerator Destroy(float time)
+    {
+        yield return new WaitForSeconds(time);
+
+        if (One == true)
+        {
+            Destroy(one);
+        }         
+        
+        if (Two == true)
+        {
+            Destroy(two);      
+        }         
+        
+        if (Three == true)
+        {
+            Destroy(three);      
+        }         
+        
+        if (Four == true)
+        {
+            Destroy(four);      
+        }
+
+        if (Five == true)
+        {
+            Destroy(five);
         }
     }
 
-    /* IEnumerator start(float Cooldown)
-     {
-         yield return new WaitForSeconds(3);
-         GameObject.Destroy(leftRight);
-     }*/
 
 }
